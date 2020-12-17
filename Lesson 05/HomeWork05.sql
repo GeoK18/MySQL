eec67b6ccb6f1bfb0bb8ad5aaadd127505e14560
GeoK18 <70892381+GeoK18@users.noreply.github.com>
2020-12-18 00:40:57 +0300

    Create HomeWork05.sql

---
+++ Lesson 05/HomeWork05.sql
@@ -0,0 +1,50 @@
+-- Практическое задание теме “Операторы, фильтрация, сортировка и ограничение”
+
+-- 1) Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
+
+REPLACE INTO users (created_at, updated_at) VALUES (NOW(), NOW());
+
+-- 2) Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданы типом VARCHAR и в них долгое время помещались значения в формате "20.10.2017 8:10". Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения
+
+UPDATE users set created_at=STR_TO_DATE(created_at, '%d.%m.%Y %H:%i'), updated_at=STR_TO_DATE(updated_at, '%d.%m.%Y %H:%i');
+ALTER TABLE users MODIFY created_at DATETIME, MODIFY updated_at DATETIME;
+
+-- 3. В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры: 0,
+-- если товар закончился и выше нуля, если на складе имеются запасы. Необходимо отсортировать записи таким образом,
+-- чтобы они выводились в порядке увеличения значения value. Нулевые запасы должны выводиться в конце, после всех записей.
+
+create table storehouses_products (
+	id SERIAL PRIMARY KEY,
+    storehouse_id INT unsigned,
+    product_id INT unsigned,
+    `value` INT unsigned COMMENT 'Остаток позиции на складе',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) COMMENT = 'Запасы на складе';
+
+INSERT INTO
+    storehouses_products (storehouse_id, product_id, value)
+VALUES
+    (1, 1, 15),
+    (1, 3, 0),
+    (1, 5, 10),
+    (1, 7, 5),
+    (1, 8, 0);
+
+SELECT 
+    value
+FROM
+    storehouses_products ORDER BY IF(value > 0, 0, 1), value;
+    
+    
+-- 4. (по желанию) Из таблицы users необходимо извлечь пользователей, родившихся в августе и мае.
+-- Месяцы заданы в виде списка английских названий ('may', 'august')   
+ 
+ SELECT name, birthday_at FROM users WHERE MONTHNAME(birthday_at) IN ('may', 'august');
+ 
+ 
+ -- 5) Из таблицы catalogs извлекаются записи при помощи запроса. SELECT * FROM catalogs WHERE id IN (5, 1, 2); Отсортируйте записи в порядке, заданном в списке IN.
+
+SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(id, 5, 1, 2);
+ 
+ 
