a1ed4e127c6c89fe3bd58a6a81ffc29330c56569
GeoK18 <70892381+GeoK18@users.noreply.github.com>
2020-12-18 00:49:24 +0300

    Update HomeWork05.sql

--- Lesson 05/HomeWork05.sql
+++ Lesson 05/HomeWork05.sql
@@ -48,3 +48,17 @@ FROM
 SELECT * FROM catalogs WHERE id IN (5, 1, 2) ORDER BY FIELD(id, 5, 1, 2);
  
  
+
+						    
+-- Практическое задание по теме “Агрегация данных”
+
+-- 1) Подсчитайте средний возраст пользователей в таблице users
+SELECT AVG(age) FROM (SELECT YEAR(CURRENT_TIMESTAMP) - YEAR(birthday) as age FROM profiles) AS Avg_age;
+
+-- 2) Подсчитайте количество дней рождения, которые приходятся на каждый из дней недели. Следует учесть, что необходимы дни недели текущего года, а не года рождения
+SELECT COUNT(*) as stats from (SELECT DAYOFWEEK(CONCAT(YEAR(NOW()),'-',MONTH(birthday),'-',DAYOFMONTH(birthday))) as date from profiles) as stats where date=1;
+
+-- 3) Подсчитайте произведение чисел в столбце таблицы
+SELECT EXP(sum(log(value))) from table;						    
+						    
+						    
